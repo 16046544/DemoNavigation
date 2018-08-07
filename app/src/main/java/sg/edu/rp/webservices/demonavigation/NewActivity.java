85db6f3e4eeaab9c12e8fc818a4def4bbb3d470d
16046544 <16046544@myrp.edu.sg>
2018-08-07 12:40:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demonavigation/NewActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.webservices.demonavigation;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+}
